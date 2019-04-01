1ba436fdc1a641dd6377fa3eeb08eff386fbbad6
Koki Imaeda <edamame_8888@yahoo.co.jp>
2019-04-01 18:11:55 +0900

    finish 2.8

--- ch2/2.8/js/app.js
+++ ch2/2.8/js/app.js
@@ -29,6 +29,17 @@ var vm = new Vue({
         return '0'
       }
       return value.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,')
-    }  
+    }
+  },
+  computed: { //算出プロパティ
+    totalPrice: function() {
+      return this.items.reduce(function(sum,item) {
+        return sum + (item.price * item.quantity)
+      },0)
+    },
+    totalPriceWithTax: function () {
+      //算出プロパティに依存した算出プロパティも定義できる
+      return Math.floor(this.totalPrice * 1.08)
+    }
   }
 });
