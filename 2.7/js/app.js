7237425b82716a9d330cee18dc139724c043962a
Koki Imaeda <edamame_8888@yahoo.co.jp>
2019-04-01 17:22:32 +0900

    create 2.7

---
+++ 2.7/js/app.js
@@ -0,0 +1,30 @@
+var items = [{
+    name: '鉛筆',
+    price: 300,
+    quantity: 0
+  },
+  {
+    name: 'ノート',
+    price: 400,
+    quantity: 0
+  },
+  {
+    name: '消しゴム',
+    price: 500,
+    quantity: 0
+  }
+];
+// ロードされ、Vueがグローバル変数として定義されているか確認
+console.assert(typeof Vue !== 'undefined');
+var vm = new Vue({
+  el: '#app',
+  data: {
+    items: items
+  },
+  filters: { //この説で追加したフィルターの追加
+    if ( !value ) {
+      return '0'
+    }
+    return value.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,')
+  }
+});
